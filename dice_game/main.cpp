fd7a5962aa78c649399dc38718da11fcbe8c7794
Amber Woodrow <amberwoodrow@gmail.com>
2015-01-14 11:51:48 -0700

    Initial Commit

---
+++ dice_game/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  dice_game
+//
+//  Created by Amber Woodrow on 1/14/15.
+//  Copyright (c) 2015 Amber Woodrow. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
